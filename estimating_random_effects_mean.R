039a37994068e0e42851433c6a56eacc3e487694
zhuob <zhuob@onid.oregonstate.edu>
2014-04-22 14:25:07 -0700

    arab datasets prepared

--- estimating_random_effects_mean.R
+++ estimating_random_effects_mean.R
@@ -17,6 +17,15 @@ group <- as.factor(c(1, 1,1, 2,2,3,3,3, 4, 4, 4))
 ## filter genes with 1 count per sample on average
 arab.filter <- as.matrix(arab.dat[rowSums(arab.dat)>= dim(arab.dat)[2], ])
 
+### prepare NB data
+library(NBPSeq)
+
+
+norm.factors <- estimate.norm.factors(arab.filter)
+lib.size <- colSums(arab.filter)
+arab.prepare <- prepare.nb.data(arab.filter, norm.factors=norm.factors)
+names(arab.prepare)
+
 #  number of genes to be sampled
 n <- 100
 set.seed(128) 
@@ -32,12 +41,32 @@ arab.sample <- arab.filter[id, ]
 vari <- c()
 
 
-for (i in 1:n){
+
+  for (i in 1:n)
+{
   y <- arab.sample[i, ]
-  a <- glmmadmb(y ~1, random=  ~1 | group, zeroInflation=F, 
-                link="log",  family = "nbinom")
+  
+  a <- glmmadmb(y ~1 + offset(log(lib.size)), random=  ~1 | group,
+                zeroInflation=F, link="log",  family = "nbinom")
+  
   vari[i] <- as.numeric(a$S)  #### output the variance
 }
+
+## considering normalization factors
+
+var.norm <- c();
+for (i in 1:n)
+{
+  y <- arab.sample[i, ]
+  
+  a <- glmmadmb(y ~1 + offset(log(arab.prepare$eff.lib.sizes)), 
+                random=  ~1 | group,
+                zeroInflation=F, link="log",  family = "nbinom")
+  
+  var.norm[i] <- as.numeric(a$S)  #### output the variance
+  
+}
+  
 
 ###  some warning message would pop up. 
 #  1: In glmmadmb(y ~ 1, random = ~1 | group, zeroInflation = F, link = "log",  :
@@ -50,28 +79,31 @@ for (i in 1:n){
 
 ## draw a histogram of the log variances.
 hist(log(vari), main=paste("number of genes =", n))
+boxplot(log(vari), log(var.norm))
 
-id2 <- which(log(vari)< -10)
+
+id2 <- which(log(var.norm)< -10)
 ## to see which genes have estimated variance of 0
-arab.pre[id[id2],]
+y <- arab.filter[id[id2],]
 
 
 # we can have glmmPQL estimate the variance, but it cannot be output.
-y <- arab.pre[id[25],]
-b <- glmmPQL(y ~1, random=  ~1 | group, family = quasipoisson)
+y <- arab.filter[id[25],]
 
-a <- glmmadmb(y~1, random=  ~1 | group, zeroInflation=F, 
+a <- glmmadmb(y~1+offset(log(lib.size)), random=  ~1 | group, zeroInflation=F, 
               link="log",  family = "nbinom")
+b <- glmmPQL(y ~1 + offset(off), random=  ~1 | group, family = quasipoisson)
+
+summary(b)
 summary(a)
+names(summary(b))
+
+# but what is the quantity under (intercetp) of Random effects part
 ## in general, a gives smaller variance than b
 
-
-
-
-
-
-
 ## resluts for the following genes are compared to NLMIXED in SAS and they are pretty close
+
+
 
 # head(arab.dat)
 # y1 <- as.numeric(arab.dat[6, ])
@@ -93,44 +125,6 @@ summary(a)
 # to check how glmmadmb  approximate the likelihood
 file.show(system.file("tpl","glmmadmb.tpl",package="glmmADMB"))
 
-
-
-
-##  to see whether it makes difference if library sizes are normalized
-library(NBPSeq)
-
-
-norm.factors <- estimate.norm.factors(arab.pre)
-
-lib.size <- colSums(arab.pre)
-max(norm.factors)/min(norm.factors) #  3.22
-max(lib.size)/min(lib.size) # 6.44
-
-
-## initializing relative frequency matrixa
-arab.filter.norm<- matrix(0, ncol= dim(arab.pre)[2], nrow = dim(arab.pre)[1])
-
-
-## multiply each count by the corresponding norm.factor and round to the 
-## nearest integer
-for ( i in 1: dim(arab.filter)[2])
-{
-  arab.filter.norm[, i]= round(arab.filter[, i]*norm.factors[i], 0)
-  
-}
-
-arab.sample.norm <- arab.filter.norm[id, ]
-
-head(arab.sample)
-head(arab.sample.norm)
-
-var.norm <- c()
-for (i in 1:n){
-  y <- arab.sample.norm[i, ]
-  a <- glmmadmb(y ~1, random=  ~1 | group, zeroInflation=F, 
-                link="log",  family = "nbinom")
-  var.norm[i] <- as.numeric(a$S)  #### output the variance
-}
 
 pdf("estimated_variance_normalized_boxplot_hist.pdf")
 hist(log(vari), main=paste("number of genes =", n))
